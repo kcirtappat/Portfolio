15f27c5a66a8c23a3cd01e5be585cac307bc9eab
kcirtappat <noscoj.pat@gmail.com>
2022-07-15 17:43:21 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//     var typed = new Typed('.auto-type', {
+//         strings: ["WELCOME"],
+//         typeSpeed: 150,
+//         backSpeed: 150,
+//         loop: true
+// })
+
+
+
+
+
+
+
+
+
+
