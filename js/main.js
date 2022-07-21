ada95ff06d2d233ca3bd402fe3fdb2d62f00296b
kcirtappat <noscoj.pat@gmail.com>
2022-07-21 13:16:11 +0800

    update color

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+var nav = document.querySelector('nav');
 
-
-
-
-
-
+window.addEventListener('scroll',() => {
+    if(window.pageYOffset > 50){
+        nav.classList.add('.fixed-top');
+    } else {
+        nav.classList.remove('.fixed-top', 'shadow');
+    }
+})
 
