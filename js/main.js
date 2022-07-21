113701aa822e52cacd5ad48372520e168970490b
kcirtappat <noscoj.pat@gmail.com>
2022-07-21 16:55:42 +0800

    portfolioTargetLinks

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-var nav = document.querySelector('nav');
-
-window.addEventListener('scroll',() => {
-    if(window.pageYOffset > 50){
-        nav.classList.add('.fixed-top');
-    } else {
-        nav.classList.remove('.fixed-top', 'shadow');
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector("#mainNav");
+    if (!navbarCollapsible) {
+      return;
     }
-})
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove("navbar-shrink");
+    } else {
+      navbarCollapsible.classList.add("navbar-shrink");
+    }
+  };
+
+  // Shrink the navbar
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
+});
+
+
+// var nav = document.querySelector('nav');
+
+// window.addEventListener('scroll',() => {
+//     if(window.pageYOffset > 50){
+//         nav.classList.add('bg-dark');
+//     } else {
+//         nav.classList.remove('bg-dark');
+//     }
+// })
+
+
 
